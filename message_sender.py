36018a907a797028aea99819c164e914ba03bfee
Mohammed Ali <mohaali482@gmail.com>
2022-03-24 09:55:03 +0300

    removed the function calling

--- message_sender.py
+++ message_sender.py
@@ -4,5 +4,3 @@ from plugins.settings import API
 def send_message (chat_id, message):
     url_req = "https://api.telegram.org/bot" + API + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + message 
     results = requests.get(url_req)
-
-send_message(chat_id="584365960", message="hello")
