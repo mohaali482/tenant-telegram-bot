03e477ec1d741c47805f2a1b6fe49d9bd1211aa6
Mohammed Ali <mohaali482@gmail.com>
2022-03-24 09:28:41 +0300

    removed prints

--- message_sender.py
+++ message_sender.py
@@ -4,6 +4,5 @@ from plugins.settings import API
 def send_message (chat_id, message):
     url_req = "https://api.telegram.org/bot" + API + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + message 
     results = requests.get(url_req)
-    print(results.json())
 
 send_message(chat_id="584365960", message="hello")
